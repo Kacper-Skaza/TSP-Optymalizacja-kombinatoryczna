0ea1d5f0346e46242e8c355060ccf5981cdc301a
Kacper Skaza <kacper.skaza.100@gmail.com>
2024-10-29 11:01:26 +0100

    Update main.cpp
    Wczytywanie i generowanie tablicy współrzędnych

--- main.cpp
+++ main.cpp
@@ -1,25 +1,101 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <ctime>
 #include <climits>
+#include <cstdlib>
 using namespace std;
 
 /*	TODO:
-	Generator -> uwaga na powtorki punktow
-	Wczytywanie z pliku -> po wspolrzednych
+	Zamiana wspolrzednych na macierz sasiedztwa
 */
 
-// graph - odleglosci miedzy poszczegolnymi miastami
-// visited - odwiedzone miasta
-// currentCity - numer aktualnego miasta
-int minDistance(vector<vector<int> > &graph, vector<bool> &visited, int currentCity) { // Szukanie najblizszego miasta
+// Wypisywanie wektora 2D
+void printVector2D(vector<vector<int>> &graph)
+{
+	for (int i=0; i<graph.size(); i++)
+	{
+		cout<<i<<": ";
+
+		for (int j=0; j<graph[i].size(); j++)
+		{
+			cout<<graph[i][j];
+			if (j != graph[i].size()-1)
+				cout<<", ";
+		}
+
+		cout<<endl;
+	}
+
+	cout<<endl;
+}
+
+// Wczytywanie tablicy wspolrzednych z pliku
+vector<vector<int>> readCoordinates(string fileName)
+{
+	ifstream inp(fileName);
+	vector<vector<int>> result;
+	int n;
+	inp>>n;
+
+	for (int i=0; i<n; i++)
+	{
+		int a=0, x=0, y=0;
+		inp>>a;
+		inp>>x;
+		inp>>y;
+		result.push_back({x, y});
+	}
+
+	inp.close();
+	return result;
+}
+
+// Generowanie tablicy wspolrzednych
+vector<vector<int>> generateCoordinates()
+{
+	srand(time( nullptr ));
+	vector<vector<int>> result;
+	int n;
+	n = (rand()%21) + 40; // Zakres: <40, 60>
+
+	for (int i=0; i<n; i++)
+	{
+		int x=0, y=0;
+		x = (rand()%2500) + 1; // Zakres: <1, 2500>
+		y = (rand()%2500) + 1; // Zakres: <1, 2500>
+
+		for (int j=0; j<result.size(); j++)
+		{
+			if (result[j][0] == x && result[j][1] == y) // Eliminacja powtorzen
+			{
+				i--;
+				continue;
+			}
+		}
+
+		result.push_back({x, y});
+	}
+
+	return result;
+}
+
+// Szukanie najblizszego miasta
+int minDistance(vector<vector<int>> &graph, vector<bool> &visited, int currentCity)
+{
+	// graph - odleglosci miedzy poszczegolnymi miastami
+	// visited - odwiedzone miasta
+	// currentCity - numer aktualnego miasta
 	int minDist;
 	int nextCity;
 
 	minDist = INT_MAX;
 	nextCity = -1;
 
-	for (int i=0; i<graph.size(); i++) {
-		if (visited[i] == false && graph[currentCity][i] < minDist) { // graph[currentCity][i] != 0
+	for (int i=0; i<graph.size(); i++)
+	{
+		if (visited[i] == false && graph[currentCity][i] < minDist)
+		{
 			minDist = graph[currentCity][i];
 			nextCity = i;
 		}
@@ -28,15 +104,22 @@ int minDistance(vector<vector<int> > &graph, vector<bool> &visited, int currentC
 	return nextCity;
 }
 
-int tspGreedy(vector<vector<int> > &graph, int currentCity) {
+// Algorytm zachlanny TSP
+int tspGreedy(vector<vector<int>> &graph, int startCity)
+{
+	cout<<"Sciezka: "<<startCity<<", ";
+
 	vector<bool> visited;
 	int totalCost;
+	int currentCity;
 
 	visited.assign(graph.size(), false);
-	visited[currentCity] = true;
+	visited[startCity] = true;
 	totalCost = 0;
+	currentCity = startCity;
 
-	for (int i=0; i<graph.size()-1; i++) {
+	for (int i=0; i<graph.size()-1; i++)
+	{
 		int nextCity = minDistance(graph, visited, currentCity);
 		totalCost += graph[currentCity][nextCity];
 		visited[nextCity] = true;
@@ -44,27 +127,62 @@ int tspGreedy(vector<vector<int> > &graph, int currentCity) {
 		cout<<currentCity<<", ";
 	}
 
-	totalCost += graph[currentCity][0]; // Powrot do miasta poczatkowego
+	totalCost += graph[currentCity][startCity]; // Powrot do miasta poczatkowego
+	cout<<startCity<<";"<<endl;
+
 	return totalCost;
 }
 
 
 
-int main() {
-	vector<vector<int> > graph; 
-    int result=0, startCity=0;
+int main()
+{
+	vector<vector<int>> graph, coordinates; 
+    int n=0, result=0, startCity=0;
 
-	graph = { // Co jesli graf nie jest pelny???
+	while (n != 1 && n != 2)
+	{
+		cout<<">> Wybierz zrodlo danych:"<<endl;
+		cout<<"1 - Plik 'Input.txt'"<<endl;
+		cout<<"2 - Generator danych"<<endl;
+		cout<<">> ";
+		cin>>n;
+		cout<<endl;
+	}
+
+	if (n == 1)
+	{
+		coordinates = readCoordinates("Input.txt");
+	}
+	else if (n == 2)
+	{
+		coordinates = generateCoordinates();
+		cout<<"Wygenerowane dane: "<<endl;
+		printVector2D(coordinates);
+	}
+
+	graph =
+	{
 		{0, 10, 15, 20},
 		{10, 0, 35, 25},
 		{15, 35, 0, 30},
 		{20, 25, 30, 0}
 	};
 
-	cout<<"Sciezka: "<<startCity<<", ";
-	result = tspGreedy(graph, startCity);
-	cout<<startCity<<endl;
-	cout<<"Minimalny koszt znaleziony przez algorytm zachlanny: "<<result;
+	cout<<">> Wybierz miasto startowe:"<<endl;
+	cout<<">> ";
+	cin>>startCity;
+	cout<<endl;
+
+	if (startCity < graph.size())
+	{
+		result = tspGreedy(graph, startCity);
+		cout<<"Minimalny koszt znaleziony przez algorytm zachlanny: "<<result;
+	}
+	else
+	{
+		cout<<"Miasto startowe nie istnieje !!!";
+	}
 
 	return 0;
 }
