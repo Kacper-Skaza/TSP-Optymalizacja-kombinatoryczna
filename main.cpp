1fa9f0cf53ef182e1399d75349b158227489bb13
Kacper Skaza <kacper.skaza.100@gmail.com>
2024-11-04 11:36:00 +0100

    Mrówki v.0.0.1

--- main.cpp
+++ main.cpp
@@ -7,6 +7,149 @@
 #include <cmath>
 using namespace std;
 
+const int MAX_CITIES = 5;        // Liczba miast (zmieñ na odpowiedni¹)
+const int NUM_ANTS = 10;         // Liczba mrówek
+const int MAX_ITERATIONS = 100;  // Maksymalna liczba iteracji
+const double ALPHA = 1.0;        // Waga œladu feromonowego
+const double BETA = 5.0;         // Waga heurystyki (odleg³oœci)
+const double RHO = 0.5;          // Wspó³czynnik parowania feromonów
+const double Q = 100.0;          // Sta³a u¿ywana do aktualizacji feromonów
+
+// Funkcja liczaca odleg³osc (tu euklidesowa)
+double distance(pair<int, int> city1, pair<int, int> city2) {
+    return sqrt(pow(city1.first - city2.first, 2) + pow(city1.second - city2.second, 2));
+}
+
+// Inicjalizowanie grafu miasta (macierz odleg³oœci)
+void initializeGraph(vector<vector<double>>& graph, vector<pair<int, int>>& cities) {
+    for (int i = 0; i < MAX_CITIES; i++) {
+        for (int j = 0; j < MAX_CITIES; j++) {
+            if (i != j) {
+                graph[i][j] = distance(cities[i], cities[j]);
+            } else {
+                graph[i][j] = 0.0;
+            }
+        }
+    }
+}
+
+// Funkcja wyboru nastêpnego miasta na podstawie prawdopodobieñstwa
+int selectNextCity(int currentCity, const vector<vector<double>>& pheromone, const vector<vector<double>>& graph, vector<bool>& visited) {
+    vector<double> probabilities(MAX_CITIES, 0.0);
+    double sum = 0.0;
+
+    for (int i = 0; i < MAX_CITIES; i++) {
+        if (!visited[i]) {
+            probabilities[i] = pow(pheromone[currentCity][i], ALPHA) * pow(1.0 / graph[currentCity][i], BETA);
+            sum += probabilities[i];
+        }
+    }
+
+    double randVal = ((double) rand() / RAND_MAX) * sum;
+    double cumulative = 0.0;
+
+    for (int i = 0; i < MAX_CITIES; i++) {
+        if (!visited[i]) {
+            cumulative += probabilities[i];
+            if (cumulative >= randVal) {
+                return i;
+            }
+        }
+    }
+
+    return -1;  // B³¹d
+}
+
+// Funkcja wykonuj¹ca trasê dla jednej mrówki
+double antTravel(int startCity, const vector<vector<double>>& graph, vector<vector<double>>& pheromone, vector<int>& bestPath, double& bestLength) {
+    vector<bool> visited(MAX_CITIES, false);
+    vector<int> path;
+    int currentCity = startCity;
+    double pathLength = 0.0;
+
+    path.push_back(currentCity);
+    visited[currentCity] = true;
+
+    for (int i = 1; i < MAX_CITIES; i++) {
+        int nextCity = selectNextCity(currentCity, pheromone, graph, visited);
+        path.push_back(nextCity);
+        visited[nextCity] = true;
+        pathLength += graph[currentCity][nextCity];
+        currentCity = nextCity;
+    }
+
+    pathLength += graph[currentCity][startCity];  // Powrót do miasta pocz¹tkowego
+    path.push_back(startCity);
+
+    // Sprawdzanie, czy znaleziono lepsz¹ trasê
+    if (pathLength < bestLength) {
+        bestPath = path;
+        bestLength = pathLength;
+    }
+
+    return pathLength;
+}
+
+// Aktualizacja feromonów
+void updatePheromones(vector<vector<double>>& pheromone, vector<vector<int>>& paths, vector<double>& lengths) {
+    // Parowanie feromonów
+    for (int i = 0; i < MAX_CITIES; i++) {
+        for (int j = 0; j < MAX_CITIES; j++) {
+            pheromone[i][j] *= (1.0 - RHO);
+        }
+    }
+
+    // Dodawanie nowych feromonów
+    for (int ant = 0; ant < NUM_ANTS; ant++) {
+        for (int i = 0; i < MAX_CITIES - 1; i++) {
+            int cityA = paths[ant][i];
+            int cityB = paths[ant][i + 1];
+            pheromone[cityA][cityB] += Q / lengths[ant];
+            pheromone[cityB][cityA] += Q / lengths[ant];  // Symetryczny graf
+        }
+    }
+}
+
+int mainmrowka() {
+    srand(time( NULL ));
+
+    // Lista miast (x, y)
+    vector<pair<int, int>> cities = {{0, 0}, {1, 5}, {5, 3}, {6, 7}, {8, 2}};
+    
+    // Macierz odleg³oœci i feromonów
+    vector<vector<double>> graph(MAX_CITIES, vector<double>(MAX_CITIES, 0.0));
+    vector<vector<double>> pheromone(MAX_CITIES, vector<double>(MAX_CITIES, 1.0));
+
+    initializeGraph(graph, cities);
+
+    vector<int> bestPath;
+    double bestLength = numeric_limits<double>::max();
+
+    for (int iteration = 0; iteration < MAX_ITERATIONS; iteration++) {
+        vector<vector<int>> paths(NUM_ANTS);
+        vector<double> lengths(NUM_ANTS);
+
+        // Symulacja dla ka¿dej mrówki
+        for (int ant = 0; ant < NUM_ANTS; ant++) {
+            int startCity = rand() % MAX_CITIES;
+            lengths[ant] = antTravel(startCity, graph, pheromone, bestPath, bestLength);
+            paths[ant] = bestPath;
+        }
+
+        // Aktualizacja feromonów
+        updatePheromones(pheromone, paths, lengths);
+    }
+
+    // Wyœwietlanie najlepszego rozwi¹zania
+    cout << "Najlepsza znaleziona trasa: ";
+    for (int city : bestPath) {
+        cout << city << " ";
+    }
+    cout << "\nD³ugoœæ trasy: " << bestLength << endl;
+
+    return 0;
+}
+
 /*	TODO:
 	Zamiana wspolrzednych na macierz sasiedztwa
 */
