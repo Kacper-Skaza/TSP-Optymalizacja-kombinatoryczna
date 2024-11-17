3e3d946fdc9769c7aa0cd0aaa906291ec23df8d2
Kacper Skaza <kacper.skaza.100@gmail.com>
2024-11-17 03:18:08 +0100

    Mrówki v.1.0.0
    Testy z rankingu included

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # TSP - Optymalizacja kombinatoryczna
-Travelling salesman problem
-Metaheurystyka --> https://www.cs.put.poznan.pl/mmachowiak/ok/
+* TSP --> *Travelling salesman problem*
+
+### Odnośniki
+* [Materiały dot. metaheurystyk](https://www.cs.put.poznan.pl/mmachowiak/ok/)
+* [Katalog instancji do rankingu](https://www.cs.put.poznan.pl/mmachowiak/instances/)
+* [Ranking](https://docs.google.com/document/d/16RDTu4t2zU7E5PF5b0aMrzgeiu99bIVZ/edit)
--- main.cpp
+++ main.cpp
@@ -1,158 +1,35 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <ctime>
-#include <climits>
+#include <iomanip>
+#include <limits>
+#include <chrono>
 #include <cstdlib>
 #include <cmath>
+#include <ctime>
 using namespace std;
 
-const int MAX_CITIES = 5;        // Liczba miast (zmieñ na odpowiedni¹)
-const int NUM_ANTS = 10;         // Liczba mrówek
-const int MAX_ITERATIONS = 100;  // Maksymalna liczba iteracji
-const double ALPHA = 1.0;        // Waga œladu feromonowego
-const double BETA = 5.0;         // Waga heurystyki (odleg³oœci)
-const double RHO = 0.5;          // Wspó³czynnik parowania feromonów
-const double Q = 100.0;          // Sta³a u¿ywana do aktualizacji feromonów
-
-// Funkcja liczaca odleg³osc (tu euklidesowa)
-double distance(pair<int, int> city1, pair<int, int> city2) {
-    return sqrt(pow(city1.first - city2.first, 2) + pow(city1.second - city2.second, 2));
-}
-
-// Inicjalizowanie grafu miasta (macierz odleg³oœci)
-void initializeGraph(vector<vector<double>>& graph, vector<pair<int, int>>& cities) {
-    for (int i = 0; i < MAX_CITIES; i++) {
-        for (int j = 0; j < MAX_CITIES; j++) {
-            if (i != j) {
-                graph[i][j] = distance(cities[i], cities[j]);
-            } else {
-                graph[i][j] = 0.0;
-            }
-        }
-    }
-}
-
-// Funkcja wyboru nastêpnego miasta na podstawie prawdopodobieñstwa
-int selectNextCity(int currentCity, const vector<vector<double>>& pheromone, const vector<vector<double>>& graph, vector<bool>& visited) {
-    vector<double> probabilities(MAX_CITIES, 0.0);
-    double sum = 0.0;
-
-    for (int i = 0; i < MAX_CITIES; i++) {
-        if (!visited[i]) {
-            probabilities[i] = pow(pheromone[currentCity][i], ALPHA) * pow(1.0 / graph[currentCity][i], BETA);
-            sum += probabilities[i];
-        }
-    }
-
-    double randVal = ((double) rand() / RAND_MAX) * sum;
-    double cumulative = 0.0;
-
-    for (int i = 0; i < MAX_CITIES; i++) {
-        if (!visited[i]) {
-            cumulative += probabilities[i];
-            if (cumulative >= randVal) {
-                return i;
-            }
-        }
-    }
-
-    return -1;  // B³¹d
-}
-
-// Funkcja wykonuj¹ca trasê dla jednej mrówki
-double antTravel(int startCity, const vector<vector<double>>& graph, vector<vector<double>>& pheromone, vector<int>& bestPath, double& bestLength) {
-    vector<bool> visited(MAX_CITIES, false);
-    vector<int> path;
-    int currentCity = startCity;
-    double pathLength = 0.0;
-
-    path.push_back(currentCity);
-    visited[currentCity] = true;
-
-    for (int i = 1; i < MAX_CITIES; i++) {
-        int nextCity = selectNextCity(currentCity, pheromone, graph, visited);
-        path.push_back(nextCity);
-        visited[nextCity] = true;
-        pathLength += graph[currentCity][nextCity];
-        currentCity = nextCity;
-    }
-
-    pathLength += graph[currentCity][startCity];  // Powrót do miasta pocz¹tkowego
-    path.push_back(startCity);
-
-    // Sprawdzanie, czy znaleziono lepsz¹ trasê
-    if (pathLength < bestLength) {
-        bestPath = path;
-        bestLength = pathLength;
-    }
-
-    return pathLength;
-}
-
-// Aktualizacja feromonów
-void updatePheromones(vector<vector<double>>& pheromone, vector<vector<int>>& paths, vector<double>& lengths) {
-    // Parowanie feromonów
-    for (int i = 0; i < MAX_CITIES; i++) {
-        for (int j = 0; j < MAX_CITIES; j++) {
-            pheromone[i][j] *= (1.0 - RHO);
-        }
-    }
-
-    // Dodawanie nowych feromonów
-    for (int ant = 0; ant < NUM_ANTS; ant++) {
-        for (int i = 0; i < MAX_CITIES - 1; i++) {
-            int cityA = paths[ant][i];
-            int cityB = paths[ant][i + 1];
-            pheromone[cityA][cityB] += Q / lengths[ant];
-            pheromone[cityB][cityA] += Q / lengths[ant];  // Symetryczny graf
-        }
-    }
-}
-
-int mainmrowka() {
-    srand(time( NULL ));
-
-    // Lista miast (x, y)
-    vector<pair<int, int>> cities = {{0, 0}, {1, 5}, {5, 3}, {6, 7}, {8, 2}};
-    
-    // Macierz odleg³oœci i feromonów
-    vector<vector<double>> graph(MAX_CITIES, vector<double>(MAX_CITIES, 0.0));
-    vector<vector<double>> pheromone(MAX_CITIES, vector<double>(MAX_CITIES, 1.0));
-
-    initializeGraph(graph, cities);
-
-    vector<int> bestPath;
-    double bestLength = numeric_limits<double>::max();
-
-    for (int iteration = 0; iteration < MAX_ITERATIONS; iteration++) {
-        vector<vector<int>> paths(NUM_ANTS);
-        vector<double> lengths(NUM_ANTS);
-
-        // Symulacja dla ka¿dej mrówki
-        for (int ant = 0; ant < NUM_ANTS; ant++) {
-            int startCity = rand() % MAX_CITIES;
-            lengths[ant] = antTravel(startCity, graph, pheromone, bestPath, bestLength);
-            paths[ant] = bestPath;
-        }
-
-        // Aktualizacja feromonów
-        updatePheromones(pheromone, paths, lengths);
-    }
-
-    // Wyœwietlanie najlepszego rozwi¹zania
-    cout << "Najlepsza znaleziona trasa: ";
-    for (int city : bestPath) {
-        cout << city << " ";
-    }
-    cout << "\nD³ugoœæ trasy: " << bestLength << endl;
-
-    return 0;
-}
-
 /*	TODO:
-	Zamiana wspolrzednych na macierz sasiedztwa
+	Milej zabawy :/
 */
+
+// CZAS
+long long Get_time()
+{
+	return chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now().time_since_epoch()).count();
+}
+
+// STROJENIE
+const int NUM_ANTS = 10;			// Liczba mrowek
+const int MAX_ITERATIONS = 100;		// Maksymalna liczba iteracji
+const double ALPHA = 1.0;			// Waga sladu feromonowego
+const double BETA = 5.0;			// Waga heurystyki (odleglosci)
+const double RHO = 0.5;				// Wspolczynnik parowania feromonow
+const double Q = 100.0;				// Stala uzywana do aktualizacji feromonow
+
+
+
+
 
 // Wypisywanie wektora 2D
 void printVector2D(vector<vector<int>> &graph)
@@ -174,11 +51,34 @@ void printVector2D(vector<vector<int>> &graph)
 	cout<<endl;
 }
 
+// Liczenie odleglosci miedzy punktami
+double calculateDistance(int x1, int x2, int y1, int y2)
+{
+	return sqrt(pow(x2-x1, 2) + pow(y2-y1, 2));
+}
+
+// Wpisywanie wartosci do macierzy sasiedztwa
+vector<vector<double>> toAdjacencyMatrix(vector<vector<int>> &graph)
+{
+	int n = graph.size();
+	vector<vector<double>> adjMatrix(n, vector<double>(n, 0.0));
+
+	for (int i=0; i<n; i++)
+	{
+		for (int j=0; j<n; j++)
+		{
+			adjMatrix[i][j] = calculateDistance(graph[i][0], graph[j][0], graph[i][1], graph[j][1]);
+		}
+	}
+
+	return adjMatrix;
+}
+
 // Wczytywanie tablicy wspolrzednych z pliku
 vector<vector<int>> readCoordinates(string fileName)
 {
 	ifstream inp(fileName);
-	vector<vector<int>> result;
+	vector<vector<int>> result(0);
 	int n;
 	inp>>n;
 
@@ -199,13 +99,13 @@ vector<vector<int>> readCoordinates(string fileName)
 vector<vector<int>> generateCoordinates(int n)
 {
 	srand(time( nullptr ));
-	vector<vector<int>> result;
+	vector<vector<int>> result(0);
 
 	for (int i=0; i<n; i++)
 	{
 		int x=0, y=0;
-		x = (rand()%2500) + 1; // Zakres: <1, 2500>
-		y = (rand()%2500) + 1; // Zakres: <1, 2500>
+		x = (rand()%1000) + 1; // Zakres: <1, 1000>
+		y = (rand()%1000) + 1; // Zakres: <1, 1000>
 
 		for (int j=0; j<result.size(); j++)
 		{
@@ -222,28 +122,9 @@ vector<vector<int>> generateCoordinates(int n)
 	return result;
 }
 
-//liczenie odleglosci miedzy punktami i wpisywanie ich do miacierzy
-double calculateDistance(int x1, int x2, int y1, int y2){
-    double distance;
-	distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));  
-	return distance;
-}
 
-//wpisywanie wartosci do macierzy sasiedztwa
-vector<vector<double>> toAdjacencyMatrix(vector<vector<int>> &graph){
-	
-	int n = graph.size();
-    vector<vector<double>> adjMatrix(n, vector<double>(n, 0));
 
-	for (int i=0; i < n; i++){
-		for (int j=0; j < n; j++){
-			double dist;
-			dist = calculateDistance(graph[i][0], graph[j][0], graph[i][1], graph[j][1]);
-			adjMatrix[i][j] = dist;
-		}
-	}
-	return adjMatrix;
-}
+
 
 // Szukanie najblizszego miasta
 int minDistance(vector<vector<double>> &graph, vector<bool> &visited, int currentCity)
@@ -251,11 +132,8 @@ int minDistance(vector<vector<double>> &graph, vector<bool> &visited, int curren
 	// graph - odleglosci miedzy poszczegolnymi miastami
 	// visited - odwiedzone miasta
 	// currentCity - numer aktualnego miasta
-	double minDist;
-	int nextCity;
-
-	minDist = INT_MAX;
-	nextCity = -1;
+	double minDist = numeric_limits<double>::max();
+	int nextCity = -1;
 
 	for (int i=0; i<graph.size(); i++)
 	{
@@ -270,47 +148,198 @@ int minDistance(vector<vector<double>> &graph, vector<bool> &visited, int curren
 }
 
 // Algorytm zachlanny TSP
-double tspGreedy(vector<vector<double>> &graph, int startCity)
+double tspGreedy(vector<vector<double>> &graph, bool showPath)
 {
-	cout<<"Sciezka: "<<startCity<<", ";
+	if (showPath == true)
+		cout<<endl<<"Znaleziona trasa: 1, ";
 
-	vector<bool> visited;
-	double totalCost;
-	int currentCity;
+	double totalCost = 0.0;
+	int currentCity = 0;
+	vector<bool> visited(graph.size(), false);
+	visited[currentCity] = true;
 
-	visited.assign(graph.size(), false);
-	visited[startCity] = true;
-	totalCost = 0;
-	currentCity = startCity;
-
-	for (int i=0; i<graph.size()-1; i++)
+	for (int i=1; i<graph.size(); i++)
 	{
 		int nextCity = minDistance(graph, visited, currentCity);
 		totalCost += graph[currentCity][nextCity];
 		visited[nextCity] = true;
 		currentCity = nextCity;
-		cout<<currentCity<<", ";
+		if (showPath == true)
+			cout<<currentCity+1<<", ";
 	}
 
-	totalCost += graph[currentCity][startCity]; // Powrot do miasta poczatkowego
-	cout<<startCity<<";"<<endl;
+	totalCost += graph[currentCity][0]; // Powrot do miasta poczatkowego
+	if (showPath == true)
+		cout<<"1;"<<endl;
 
 	return totalCost;
 }
 
 
 
+
+
+// Funkcja wyboru nastepnego miasta na podstawie prawdopodobienstwa
+int selectNextCity(int currentCity, vector<vector<double>> &pheromone, vector<vector<double>> &graph, vector<bool> &visited)
+{
+	int n = graph.size();
+
+	vector<double> probabilities(n, 0.0);
+	double sum = 0.0;
+
+	for (int i=0; i<n; i++)
+	{
+		if (visited[i] == false)
+		{
+			probabilities[i] = pow(pheromone[currentCity][i], ALPHA) * pow(1.0 / graph[currentCity][i], BETA);
+			sum += probabilities[i];
+		}
+	}
+
+	double randVal = ((double) rand() / RAND_MAX) * sum;
+	double cumulative = 0.0;
+
+	for (int i=0; i<n; i++)
+	{
+		if (visited[i] == false)
+		{
+			cumulative += probabilities[i];
+			if (cumulative >= randVal)
+				return i;
+		}
+	}
+
+	return -1; // Error
+}
+
+// Funkcja wykonujaca trase dla jednej mrowki
+double antTravel(int startCity, vector<vector<double>> &graph, vector<vector<double>> &pheromone, vector<int> &bestPath, double &bestLength)
+{
+	int n = graph.size();
+
+	vector<bool> visited(n, false);
+	vector<int> path(0);
+	int currentCity = startCity;
+	double pathLength = 0.0;
+
+	path.push_back(currentCity);
+	visited[currentCity] = true;
+
+	for (int i=1; i<n; i++)
+	{
+		int nextCity = selectNextCity(currentCity, pheromone, graph, visited);
+		path.push_back(nextCity);
+		visited[nextCity] = true;
+		pathLength += graph[currentCity][nextCity];
+		currentCity = nextCity;
+	}
+
+	pathLength += graph[currentCity][startCity]; // Powrot do miasta pocz¹tkowego
+	path.push_back(startCity);
+
+	// Sprawdzanie, czy znaleziono lepsza trase
+	if (pathLength < bestLength)
+	{
+		bestPath = path;
+		bestLength = pathLength;
+	}
+
+	return pathLength;
+}
+
+// Aktualizacja feromonow
+void updatePheromones(vector<vector<double>> &pheromone, vector<vector<int>> &paths, vector<double> &lengths)
+{
+	int n = pheromone.size();
+
+	// Parowanie feromonow
+	for (int i=0; i<n; i++)
+	{
+		for (int j=0; j<n; j++)
+		{
+			pheromone[i][j] *= (1.0 - RHO);
+		}
+	}
+
+	// Dodawanie nowych feromonow
+	for (int ant=0; ant<NUM_ANTS; ant++)
+	{
+		for (int i=0; i<n-1; i++)
+		{
+			int cityA = paths[ant][i];
+			int cityB = paths[ant][i+1];
+			pheromone[cityA][cityB] += Q / lengths[ant];
+			pheromone[cityB][cityA] += Q / lengths[ant]; // Symetria
+		}
+	}
+}
+
+// Algorytm mrowkowy TSP
+double tspAntColony(vector<vector<double>> &graph, bool showPath)
+{
+	srand(time( nullptr ));
+
+	int n = graph.size();
+
+	vector<vector<double>> pheromone(n, vector<double>(n, 1.0));
+
+	vector<int> bestPath;
+	double bestLength = numeric_limits<double>::max();
+
+	for (int i=0; i<MAX_ITERATIONS; i++)
+	{
+		vector<vector<int>> paths(NUM_ANTS);
+		vector<double> lengths(NUM_ANTS);
+
+		// Symulacja dla kazdej mrowki
+		for (int ant=0; ant<NUM_ANTS; ant++)
+		{
+			int startCity = rand() % n;
+			lengths[ant] = antTravel(startCity, graph, pheromone, bestPath, bestLength);
+			paths[ant] = bestPath;
+		}
+
+		// Aktualizacja feromonow
+		updatePheromones(pheromone, paths, lengths);
+	}
+
+	if (showPath == true)
+	{
+		cout<<endl<<"Znaleziona trasa:";
+		for (int city : bestPath)
+		{
+			cout << city << " ";
+		}
+	}
+
+	return bestLength;
+}
+
+
+
+
+
 int main()
 {
-	vector<vector<int>> coordinates; 
-    int n=0, m=0, startCity=0;
-	double result=0;
+	srand(time( nullptr ));
 
-	while (n != 1 && n != 2)
+	vector<vector<int>> coordinates(0);
+	vector<vector<double>> graph(0);
+	long long time_1=0, time_2=0;
+	double result=0;
+	int n=0, m=0;
+
+	while (n < 1 || n > 8)
 	{
 		cout<<">> Wybierz zrodlo danych:"<<endl;
-		cout<<"1 - Plik 'Input.txt'"<<endl;
-		cout<<"2 - Generator danych"<<endl;
+		cout<<"1 - Ranking: 'berlin52.txt'"<<endl;
+		cout<<"2 - Ranking: 'bier127.txt'"<<endl;
+		cout<<"3 - Ranking: 'tsp250.txt'"<<endl;
+		cout<<"4 - Ranking: 'tsp500.txt'"<<endl;
+		cout<<"5 - Ranking: 'tsp1000.txt'"<<endl;
+		cout<<"6 - Plik 'Test.txt'"<<endl;
+		cout<<"7 - Plik 'Input.txt'"<<endl;
+		cout<<"8 - Generator danych"<<endl;
 		cout<<">> ";
 		cin>>n;
 		cout<<endl;
@@ -318,49 +347,62 @@ int main()
 
 	if (n == 1)
 	{
-		coordinates = readCoordinates("Input/Input.txt");
+		coordinates = readCoordinates("Data/Ranking/berlin52.txt");
 	}
 	else if (n == 2)
+	{
+		coordinates = readCoordinates("Data/Ranking/bier127.txt");
+	}
+	else if (n == 3)
+	{
+		coordinates = readCoordinates("Data/Ranking/tsp250.txt");
+	}
+	else if (n == 4)
+	{
+		coordinates = readCoordinates("Data/Ranking/tsp500.txt");
+	}
+	else if (n == 5)
+	{
+		coordinates = readCoordinates("Data/Ranking/tsp1000.txt");
+	}
+	else if (n == 6)
+	{
+		coordinates = readCoordinates("Data/Test.txt");
+	}
+	else if (n == 7)
+	{
+		coordinates = readCoordinates("Data/Input.txt");
+	}
+	else if (n == 8)
 	{
 		cout<<">> Podaj ilosc miast:"<<endl;
 		cout<<">> ";
 		cin>>m;
+		cout<<endl;
+
 		coordinates = generateCoordinates(m);
-		cout<<"Wygenerowane dane: "<<endl;
+		cout<<">> Wygenerowane dane: "<<endl;
 		printVector2D(coordinates);
 	}
 
-	vector<vector<double>> graph = toAdjacencyMatrix(coordinates);
-	// for (const auto& row : graph) {
-    //     cout << "{";
-    //     for (size_t i = 0; i < row.size(); ++i) {
-    //         cout << row[i];
-    //         if (i < row.size() - 1) {
-    //             cout << ",";
-    //         }
-    //     }
-    //     cout << "}" << endl; }
-	// graph =
-	// {
-	// 	{0, 10, 15, 20},
-	// 	{10, 0, 35, 25},
-	// 	{15, 35, 0, 30},
-	// 	{20, 25, 30, 0}
-	// };
-
-	cout<<">> Wybierz miasto startowe:"<<endl;
-	cout<<">> ";
-	cin>>startCity;
-	cout<<endl;
-
-	if (startCity < graph.size())
+	if (coordinates.size() > 0)
 	{
-		result = tspGreedy(graph, startCity);
-		cout<<"Minimalny koszt znaleziony przez algorytm zachlanny: "<<result<< endl;
+		graph = toAdjacencyMatrix(coordinates);
+		cout<<fixed<<showpoint<<"Minimalny koszt znaleziony przez:"<<endl;
+
+		time_1 = Get_time();
+		result = tspGreedy(graph, false); // false/true -> wyswietlanie znalezionej trasy
+		time_2 = Get_time() + 1;
+		cout<<">> Algorytm zachlanny ["<<setprecision(3)<<(time_2-time_1)/1000.0<<"s]: "<<setprecision(2)<<result<<endl;
+
+		time_1 = Get_time();
+		result = tspAntColony(graph, false); // false/true -> wyswietlanie znalezionej trasy
+		time_2 = Get_time() + 1;
+		cout<<">> Algorytm mrowkowy ["<<setprecision(3)<<(time_2-time_1)/1000.0<<"s]: "<<setprecision(2)<<result;
 	}
 	else
 	{
-		cout<<"Miasto startowe nie istnieje !!!";
+		cout<<"Pusty graf !!!";
 	}
 
 	return 0;
